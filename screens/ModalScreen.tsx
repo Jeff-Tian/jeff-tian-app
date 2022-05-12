106cfa94f6a8b76316101c5635d3f1e2ff8210f5
Jeff Tian <jeff.tian@outlook.com>
2022-05-12 17:46:04 +0800

    ci: display package version and datetime

--- screens/ModalScreen.tsx
+++ screens/ModalScreen.tsx
@@ -4,10 +4,13 @@ import { Platform, StyleSheet } from 'react-native';
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
+import pack from '../package.json'
+
 export default function ModalScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Modal</Text>
+      <Text style={styles.title}>Jeff Tian App</Text>
+      <Text>Version: {pack.version}</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/ModalScreen.tsx" />
 
